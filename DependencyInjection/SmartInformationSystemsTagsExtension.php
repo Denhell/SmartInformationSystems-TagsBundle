35fbb0bb9deacbd70c7502e75206497db8dea91f
YK115 <kurzhumov@gmail.com>
2014-06-03 14:57:14 +0600

    Update SmartInformationSystemsTagsExtension.php

--- DependencyInjection/SmartInformationSystemsTagsExtension.php
+++ DependencyInjection/SmartInformationSystemsTagsExtension.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace SmartSystems\TagsBundle\DependencyInjection;
+namespace SmartInformationSystems\TagsBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class SmartSystemsTagsExtension extends Extension
+class SmartInformationSystemsTagsExtension extends Extension
 {
     /**
      * {@inheritDoc}
